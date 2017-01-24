9563a8293f7a73b31272cb94627be4156584a13f
lemonjai <leocheung_1986@hotmail.com>
2017-01-23 23:33:26 -0500

    add the gravatar

--- app/assets/frontend/component/Tweet.jsx
+++ app/assets/frontend/component/Tweet.jsx
@@ -3,7 +3,7 @@ export default class Tweet extends React.Component {
     return(
       <div>
         <li className="collection-item avatar">
-          <i className="material-icons circle">person_pin</i>
+          <img className="circle" src={this.props.gravatar} />
           <span className="title">{this.props.name}</span>
           <time>{this.props.formattedDate }</time>
           <p>{this.props.body}</p>
--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -313,11 +313,7 @@
 	        React.createElement(
 	          "li",
 	          { className: "collection-item avatar" },
-	          React.createElement(
-	            "i",
-	            { className: "material-icons circle" },
-	            "person_pin"
-	          ),
+	          React.createElement("img", { className: "circle", src: this.props.gravatar }),
 	          React.createElement(
 	            "span",
 	            { className: "title" },
